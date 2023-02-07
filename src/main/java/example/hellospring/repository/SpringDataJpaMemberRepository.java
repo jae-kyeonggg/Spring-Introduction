5c88c12439da2a0b359502b0a884eae66d3de25d
jae-kyeonggg <hanjaekyung56@gmail.com>
2023-02-07 20:28:31 +0900

    Spring Data Jpa

---
+++ src/main/java/example/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,12 @@
+package example.hellospring.repository;
+
+import example.hellospring.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository{
+    //JPQL select m from Member m where m.name = ?
+    @Override
+    Optional<Member> findByName(String name);
+}
