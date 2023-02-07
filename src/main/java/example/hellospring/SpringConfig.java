8dc10bd8836e1aafb2bf19d7ae0dbb89518e11b2
jae-kyeonggg <hanjaekyung56@gmail.com>
2023-02-07 20:27:16 +0900

    Spring Data Jpa

--- src/main/java/example/hellospring/SpringConfig.java
+++ src/main/java/example/hellospring/SpringConfig.java
@@ -1,38 +1,41 @@
 package example.hellospring;
 
-import example.hellospring.repository.JpaMemberRepository;
 import example.hellospring.repository.MemberRepository;
 import example.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    private final MemberRepository memberRepository;
 
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+
+    /*private DataSource dataSource;
     private EntityManager em;
 
     @Autowired
     public SpringConfig(DataSource dataSource, EntityManager em){
         this.dataSource = dataSource;
         this.em = em;
-    }
+    }*/
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
+    /*@Bean
     public MemberRepository memberRepository(){
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
         //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+        //return new JpaMemberRepository(em);
+    }*/
 }
